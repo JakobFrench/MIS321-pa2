da0cc89a0acc22e3ca149f01674e5fb753e317ee
Jake French <jakeyfrench@Jakes-MacBook-Pro-3.local>
2022-03-01 12:18:56 -0600

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pa2_version2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
